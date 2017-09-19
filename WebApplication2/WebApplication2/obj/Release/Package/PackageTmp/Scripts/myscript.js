352a3ecf55a3f824af05bdc37b5d2d7273d55aba
verywarmsound <imkarpacz@gmail.com>
2017-09-19 16:19:37 +0200

    y
    g

---
+++ WebApplication2/WebApplication2/obj/Release/Package/PackageTmp/Scripts/myscript.js
@@ -0,0 +1,15 @@
+﻿$(function () {
+ 
+    "use strict";
+    var wheight = $(window).height(); //get the height of the window
+
+    $('.fullheight').css('height', wheight); //set to window tallness  
+
+
+    //adjust height of .fullheight elements on window resize
+    $(window).resize(function () {
+        wheight = $(window).height(); //get the height of the window
+        $('.fullheight').css('height', wheight); //set to window tallness  
+    });
+
+})
